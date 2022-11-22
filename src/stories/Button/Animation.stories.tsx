a4fd337d310c9d831a282d6808d42da458bef8f3
61130061 <thezeroyou@gmail.com>
2022-11-22 16:41:00 +0700

    ♻️ update Basic Button component

--- src/stories/Button/Animation.stories.tsx
+++ src/stories/Button/Animation.stories.tsx
@@ -10,7 +10,7 @@ export default {
   title: 'Button/Animation',
 };
 
-export const Fill = ({ label }: ButtonProps) => {
+export const Primary = ({ label }: ButtonProps) => {
   return (
     <button 
       className="px-5 py-2 bg-indigo-500 outline-none rounded text-white shadow-indigo-700 shadow-md font-medium active:shadow-none active:scale-95 hover:bg-indigo-600 focus:bg-indigo-600 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
@@ -19,7 +19,7 @@ export const Fill = ({ label }: ButtonProps) => {
     </button>
   )
 }
-Fill.args = {
+Primary.args = {
   label: 'Button'
 };
 
--- src/stories/Button/Basic.stories.tsx
+++ src/stories/Button/Basic.stories.tsx
@@ -10,15 +10,30 @@ export default {
   title: 'Button/Basic',
 };
 
-export const Fill = ({ label }: ButtonProps) => {
+export const Primary = ({ label }: ButtonProps) => {
   return (
-    <button 
-      className="p-3 border rounded-lg"
+    <button
+      type="button"
+      className="bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
     >
       {label}
     </button>
   )
 }
-Fill.args = {
-  label: 'Hello'
+Primary.args = {
+  label: 'Button'
+};
+
+export const Outline = ({ label }: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className="border-2 border-indigo-500 text-indigo-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:border-indigo-600 hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+    >
+      {label}
+    </button>
+  )
+}
+Outline.args = {
+  label: 'Button'
 };
