108bc1f89c404cdd0d6e9392c1e23d63868902ce
61130061 <thezeroyou@gmail.com>
2022-12-20 14:30:15 +0700

    ➕ add Radio component

---
+++ src/stories/Input/Radio.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+interface RadioProps {
+}
+
+export default {
+  title: 'Input/Radio',
+};
+
+const options = ['Everything', 'Same as email', 'No push notifications'];
+
+export const Radio = ({ }: RadioProps) => {
+
+  return (
+    <fieldset>
+      <legend className="contents text-base font-medium text-gray-900">Push Notifications</legend>
+      <p className="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
+      <div className="mt-4 space-y-4">
+        {options.map((d, i) => 
+          <div key={i} className="flex items-center">
+            <input
+              id={"push-everything-"+i}
+              name="push-notifications"
+              type="radio"
+              className="appearance-none mr-3 h-3 w-3 rounded-full text-indigo-600 checked:bg-blue-500 ring-gray-300 ring-offset-2 ring-2 checked:ring-blue-500"
+            />
+            <label htmlFor={"push-everything-"+i} className="block text-sm font-medium text-gray-700">
+              {d}
+            </label>
+          </div>
+        )}
+      </div>
+    </fieldset>
+  )
+}
