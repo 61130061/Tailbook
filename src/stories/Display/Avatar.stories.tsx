cfd445991f766b344a39c46c434cdc59929711c3
61130061 <thezeroyou@gmail.com>
2022-12-20 10:03:32 +0700

    ♻️ update Avatar component

--- src/stories/Display/Avatar.stories.tsx
+++ src/stories/Display/Avatar.stories.tsx
@@ -51,7 +51,7 @@ export const Profile = ({ circle }: AvatarProps) => {
   return (
     <div className="flex gap-3 items-center">
       <img
-        className={`inline-block h-10 w-10 rounded-${circle ? "full" : "md"} ring-2 ring-white`}
+        className={`inline-block h-14 w-14 rounded-${circle ? "full" : "md"} ring-2 ring-white`}
         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         alt=""
       />
