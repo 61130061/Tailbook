62368cfb19898b05bd5fda014f882c3ee49ba44f
61130061 <thezeroyou@gmail.com>
2023-01-04 10:47:54 +0700

    ♻️ remove unnecessary class in checkbox style

--- src/stories/Input/Checkbox.stories.tsx
+++ src/stories/Input/Checkbox.stories.tsx
@@ -15,7 +15,7 @@ export const Checkbox = ({ }: CheckboxProps) => {
       <input
         id="checkbox"
         type="checkbox"
-        className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded accent-gray-700"
+        className="h-4 w-4 accent-gray-700"
       />
       <label htmlFor="checkbox" className="text-sm ml-3 font-medium text-gray-900">I agree to the <a href="#" className="text-blue-600 hover:underline">terms and conditions</a></label>
     </div>
