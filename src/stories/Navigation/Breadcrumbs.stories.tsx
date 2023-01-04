7d434e15c7c1a2e552b60d7808617cfd8514b302
61130061 <thezeroyou@gmail.com>
2023-01-04 10:40:31 +0700

    ♻️  update Breadscrumbs style

--- src/stories/Navigation/Breadcrumbs.stories.tsx
+++ src/stories/Navigation/Breadcrumbs.stories.tsx
@@ -2,52 +2,82 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 interface BreadProps {
-  bgColor?: boolean,
 }
 
 export default {
   title: 'Navigation/Breadcrumbs',
 };
 
-export const Breadcrumbs = ({ bgColor }: BreadProps) => {
+export const Breadcrumbs = ({ }: BreadProps) => {
 
-  let child = ['home', 'setting', 'customization', 'blue'];
-  let containerCSS = "flex gap-2 text-indigo-400 rounded-xl";
-  let linkCSS = "hover:underline hover:cursor-pointer hover:underline-offset-2"
-  let currentCSS = "font-semibold" 
-
-  if (bgColor) containerCSS+=" bg-indigo-50 px-5 py-3";
+  const buttonCSS = `hover:bg-slate-50 hover:text-sky-700 px-2 py-1 rounded-lg`
 
   return (
-    <div
-      className={containerCSS}
-    >
-      {child.map((d, i) =>
-        <div className="flex gap-2 items-center capitalize">
-          <a
-            className={i < child.length-1 ? linkCSS : currentCSS}
-            key={i}
+    <div className="h-[320px]">
+      <div className="flex gap-1 items-center text-gray-600 text-sm font-medium">
+        <button className={buttonCSS}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+            <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+            <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+          </svg>
+        </button>
+        <div className="text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            strokeWidth={3.5} stroke="currentColor"
+            className="w-4 h-4"
           >
-            {d}
-          </a>
-          {i < child.length - 1 &&
-            <div
-              className="fill-indigo-200 stroke-indigo-200 stroke-[10px]"
-            >
-              <svg 
-                version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 330.002 330.002"
-                width="13px" 
-                height="13px"
-              >
-                <path id="XMLID_226_" d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21	l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001	c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"/>
-              </svg>
-            </div>
-          }
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
         </div>
-      )}
+        <button className={buttonCSS}>Level one</button>
+        <div className="text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            strokeWidth={3.5} stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+        <div className="relative">
+          <button className={buttonCSS}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-5 h-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+          </button>
+          <div className="absolute py-2 mt-1 rounded-lg shadow -left-[30px] w-[120px]">
+            {['two', 'three', 'four', 'five'].map((d, i) => 
+              <div 
+                key={i} 
+                className="px-3 py-2 hover:bg-slate-100 hover:cursor-pointer"
+              >
+                Level {d}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            strokeWidth={3.5} stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+        <button className={buttonCSS}>Level six</button>
+        <div className="text-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            strokeWidth={3.5} stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+        <div className="px-2 py-1 rounded-lg font-bold text-gray-700">
+          Active
+        </div>
+      </div>
     </div>
   )
 }
 Breadcrumbs.args = {
-  bgColor: true
 };
