fd7c113e9c268524484b811da3f57827b68b0a3e
61130061 <thezeroyou@gmail.com>
2023-01-05 11:17:44 +0700

    ➕ add Radio Card & update Radio component

--- src/stories/Input/Radio.stories.tsx
+++ src/stories/Input/Radio.stories.tsx
@@ -14,20 +14,20 @@ const options = ['Everything', 'Same as email', 'No push notifications'];
 export const Radio = ({ labelColor }: RadioProps) => {
 
   return (
-    <fieldset>
-      <legend className="contents text-base font-medium text-gray-900">Push Notifications</legend>
+    <div>
+      <div className="contents text-base font-medium text-gray-900">Push Notifications</div>
       <p className="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
       <div className="mt-4 space-y-4">
         {options.map((d, i) => 
           <div key={i} className="flex items-center">
             <input
-              id={"push-everything-"+i}
+              id={"id-"+i}
               name="push-notifications"
               type="radio"
               className={`${labelColor ? 'peer ':''}appearance-none mr-3 h-3 w-3 rounded-full checked:bg-blue-400 ring-gray-300 ring-offset-1 ring-2 checked:ring-blue-300 hover:cursor-pointer`}
             />
             <label 
-              htmlFor={"push-everything-" + i}
+              htmlFor={"id-" + i}
               className={`block hover:cursor-pointer text-sm font-medium text-gray-700${labelColor ? ' peer-checked:text-blue-500':''}`}
             >
               {d}
@@ -35,7 +35,7 @@ export const Radio = ({ labelColor }: RadioProps) => {
           </div>
         )}
       </div>
-    </fieldset>
+    </div>
   )
 }
 Radio.args = {
---
+++ src/stories/Input/RadioCard.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'Input/Radio Card',
+};
+
+const demoData = [
+  {
+    title: 'Relaxed',
+    desc: 'New campaign sent two months'
+  },
+  {
+    title: 'Normal',
+    desc: 'New campaign sent every month'
+  },
+  {
+    title: 'Accelerated',
+    desc: 'New campaign sent every two weeks'
+  }
+]
+
+export const RadioCard = () => {
+
+  return (
+    <div className="grid grid-cols-3 gap-x-2">
+      {demoData.map((d, i) =>
+        <div className="relative flex">
+          <input
+            id={"id-" + i}
+            name="push-notifications"
+            type="radio"
+            checked="true"
+            className={`peer appearance-none mr-3 h-3 w-3 rounded-full checked:bg-gray-800 ring-gray-300 ring-offset-2 ring-1 checked:ring-gray-600 hover:cursor-pointer absolute right-3 top-5`}
+          />
+          <label
+            htmlFor={"id-" + i}
+            className="w-full bg-white border rounded-lg px-5 py-3 hover:cursor-pointer peer-checked:border-gray-700 peer-checked:bg-gray-50"
+          >
+            <div className="text-gray-800 font-medium">{d.title}</div>
+            <div className="text-gray-600 text-sm">{d.desc}</div>
+          </label>
+
+        </div>
+      )}
+    </div>
+  )
+}
