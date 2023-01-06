772532bc4e28abbb30bf45c9f9e92cc39b795ed0
61130061 <thezeroyou@gmail.com>
2023-01-06 11:25:29 +0700

    ➕ add theming addon to change logo

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import tailbookTheme from './theme';
+
+addons.setConfig({
+  theme: tailbookTheme,
+});
---
+++ .storybook/theme.js
@@ -0,0 +1,6 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  brandTitle: 'Tailbook',
+  brandImage: 'https://raw.githubusercontent.com/61130061/Tailbook/master/docs/tailbook_logo.png',
+});
