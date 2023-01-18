64f73dd73435eeb6c975dd7f33f11a0001b1871c
61130061 <thezeroyou@gmail.com>
2023-01-18 17:41:06 +0700

    🐣 initial commit docusaurus + tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+
+  return (
+    <header className="bg-emerald-400 h-96">
+      <div className="flex justify-center items-center h-full">
+        <h1>Tailbook w/ Docusaurus</h1>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
